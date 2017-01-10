5148b360b427bf011671753d21d13419bbba66b6
Tom Smith <tom.smith@kudan.eu>
2017-01-10 14:42:41 +0000

    Updated app id and api key

--- app/src/main/java/eu/kudan/cms/demo/MainActivity.java
+++ app/src/main/java/eu/kudan/cms/demo/MainActivity.java
@@ -20,7 +20,7 @@ public static String packageName;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         ARAPIKey key = ARAPIKey.getInstance();
-        key.setAPIKey("GAXAE-F9AU4-58C4L-35HMV-EY6C9-GD6XG-2RJKX-BACQZ-A9KQB-XYYXC-7LCB2-8UUN2-FEXW5-W6CVL-27QYS-QU");
+        key.setAPIKey("sVmoznmKZ+4nFEHD6HoslwpC26PNuBZGHrikUwyon2BKSvza1yu2CqbSrae+pHPr1NHjhsf5pHQOZn8IEqXlqXFodGsrOJhxJANbMOdvnRLUi9/QWGqyRL9FViDmyohw6e5R7U4Ex8H7d7spLLvhfp5HFv56DgLr8c8sC2ipDtv9g1IjOTaY7UGxata3eulG2A/UkIdRv2NcotZXqan01xQUWFAislEwlGguParEYiwu11T4mqtU3dQBbfxpvxbczjdYz493YG3rAO2RHgT+5M5TJShJsz2irkNo71JD2Fzqf4AR2b4+7t1c55zKjegXzGS6Xa/rpNn9yiXUn7rUYIHNvN3cEQa9HsZiVxAV4vJgxFS+T/AxfWqKrEg1uj6xF5MsodZ2EkZ8mqliYIsxZqnFz+Re2HeWG8wvrEob0ZwRIO0TxppAemZc3HChTAPLcNt5gzeBk0oRP4wnrFAFFBDi8XjDocwTSVw++hWZb1qNHzt6bKLsMDRT057UVuuZB6M8f7EOQD79Oah0Vrx/3DUK6e9BEV8oGFNHtk1wyYEkg0i6RLhVSokGx//Qj36A4gCz3h1OjtfB0OuukbNq7xI1L/FcNQLmGYNGZwszARjGr9ESw1gVAkbQMxaV27uo/KoIq4+nR7RL8iT7t7NAaXCFIi24RR+7WGjTvKqWYjA=");
         packageName = getApplicationContext().getPackageName();
         setContentView(R.layout.activity_main);
         permissionsRequest();
